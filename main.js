42085efd0d4f778d8bfa70175b7dbbd637566984
JungGo <khatawut.ptk@gmail.com>
2026-03-17 11:22:28 +0700

    Refactor code structure and remove redundant changes

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-// import { 
-//     requestLogger, 
-//     errorLogger 
-// } from './middleware/logger.js';
+import { 
+    requestLogger, 
+    errorLogger 
+} from './src/middleware/logger.js';
 
 
 dotenv.config();
@@ -13,11 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use(requestLogger);
+app.use(requestLogger);
 
 
-
-// app.use(errorLogger);
+app.use(errorLogger);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
